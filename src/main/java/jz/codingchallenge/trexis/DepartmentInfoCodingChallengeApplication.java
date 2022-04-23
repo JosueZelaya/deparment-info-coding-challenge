05525e9fc033431716113976d841b34754b3b659
JosueZelaya <josue.zelaya@praxent.com>
2022-04-23 12:54:45 -0600

    spring boot app up and running

---
+++ src/main/java/jz/codingchallenge/trexis/DepartmentInfoCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package jz.codingchallenge.trexis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentInfoCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentInfoCodingChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jz/codingchallenge/trexis/DepartmentInfoCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package jz.codingchallenge.trexis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentInfoCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
